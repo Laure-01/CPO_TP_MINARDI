98cfaad8239403e8c35bca47776938fcaaa431df
lenovo <lenovo@10.3.61.207>
2024-09-17 12:58:03 +0200

    Création d'un nouveau projet versionné"

---
+++ TP0_Exo2_/src/tp0_exo2_/TP0_Exo2_.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_exo2_;
+
+/**
+ *
+ * @author lenovo
+ */
+public class TP0_Exo2_ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       public static void main(String[] args) {
+//Declaration des variables
+int nb; // nombre dentiers a additionner
+int result; // resultat
+int ind; //indice
+nb=5;
+result=0;
+// Addition des nb premiers entiers
+ind=1;
+while (ind <= nb) {
+result=result+ind;
+}
+// Affichage du resultat
+System.out.println();
+System.out.println("La somme des "+ nb + "entiers est: "+result);
+}
+    }
+    
+}
