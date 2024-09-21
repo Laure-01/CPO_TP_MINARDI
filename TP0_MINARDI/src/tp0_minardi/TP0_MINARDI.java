d81e38968d50a9a44b7192aad274d88d75f68cd5
lenovo <lenovo@10.3.61.207>
2024-09-21 16:56:14 +0200

    Création d'un nouveau projet versionné"

---
+++ TP0_MINARDI/src/tp0_minardi/TP0_MINARDI.java
@@ -0,0 +1,26 @@
+/*
+ * TDB 
+ * septembre 2024
+ */
+package tp0_minardi;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lenovo
+ */
+public class TP0_MINARDI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Au revoir ");
+   String prenom;
+Scanner sc;
+sc = new Scanner(System.in);
+System.out.println("Bonjour, quel est votre age ?");
+prenom = sc.nextLine(); }
+   
+}
