6ff74c459113b512895758b7e64680e00508deeb
lenovo <lenovo@10.3.61.207>
2024-09-27 11:11:01 +0200

    Création d'un nouveau projet versionné"

---
+++  Tp1_stats_laure/src/tp1_stats_laure/Tp1_stats_laure.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_stats_laure;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Tp1_stats_laure {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int [] Tab =new int[6];
+        Scanner sc = new Scanner(System.in);
+        Random generateurAleat = new Random(); // Initialiser le générateur aléatoire
+
+        System.out.print("Saisissez une valeur (m) : ");
+        int m = sc.nextInt(); // Lire la valeur de m
+        for (int i=0;i<m;i++){
+            int alea= generateurAleat.nextInt(6);
+            Tab[alea]= Tab[alea]+1; // incrémenter la case correspondant dans le tableau
+        }
+        System.out.println("Tableau des occurences"+Tab);
+    }
+    
+}
